e6b380148fa5e515d83485dd6f27fba9acfaacb8
guozhao <davidkoojohn@163.com>
2021-12-13 20:59:39 +0800

    add react-redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter } from "react-router-dom"
-import './index.css'
-import App from './App/App'
-import "tailwindcss/tailwind.css"
-
+import { Provider } from "react-redux"
 import store from "./store"
-import { addToCart, updateCart, deleteFromCart } from "./store/actions/cart_actions"
-
-console.log("initial state: ", store.getState());
-
-let unsubscribe = store.subscribe(() => {
-  console.log("store change:", store.getState());
-})
-
-store.dispatch(addToCart('Coffee 500gm', 1, 250));
-store.dispatch(addToCart('Flour 1kg', 2, 110));
-store.dispatch(addToCart('Juice 2L', 1, 250));
-
-store.dispatch(updateCart("Coffee 500gm", 111, 23))
-store.dispatch(deleteFromCart("Flour 1kg"))
-
-unsubscribe()
+import App from './App/App'
+import './index.css'
+import "tailwindcss/tailwind.css"
 
 ReactDOM.render(
   <HashRouter>
     <React.StrictMode>
-      <App />
+      <Provider store={ store }>
+        <App />
+      </Provider>
     </React.StrictMode>
   </HashRouter>,
   document.getElementById('root')
