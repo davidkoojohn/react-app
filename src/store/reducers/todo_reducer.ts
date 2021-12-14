5fe9263752e55582a41c79f6accf745f5625872e
guozhao <davidkoojohn@163.com>
2021-12-14 09:21:41 +0800

    add todo reducer method

--- src/store/reducers/todo_reducer.ts
+++ src/store/reducers/todo_reducer.ts
@@ -15,5 +15,25 @@ const initialState = [
 ] as TodoItem[]
 
 export default function (state = initialState, action: any) {
-  return initialState
+  switch (action.type) {
+    case ADD_TODO: {
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ]
+    }
+    case TOGGLE_TODO: {
+      return state.map(item =>
+        item.id === action.id
+          ? {...item, completed: !item.completed}
+          : item
+      )
+    }
+    default:
+      return state
+  }
 }
